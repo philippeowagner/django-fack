2b9f02fa078ca1f4de6e3e623974b1c375be7e09
Philippe O. Wagner <wagner@arteria.ch>
2016-04-13 16:17:39 +0200

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = 'django-fack',
-    version = '1.1',
+    version = '1.1.1',
     description = 'A simple FAQ application for Django sites.',
     long_description = read('README.rst'),
     license = "BSD",
